5986b19d11080dc754b009e1f9433b2df972e50f
saipunya <sumetchoorat@gmail.com>
2025-05-01 12:10:06 +0700

    update first time

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+// app set
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// app use
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+// middleware
+app.use(cors());
+app.use(bodyParser.json());
+// routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
