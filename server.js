f26cad63980d01b2c5d4b8f172d8082ae537638c
saipunya <sumetchoorat@gmail.com>
2025-07-25 09:46:59 +0700

    update

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.set('views', path.join(__dirname, 'views'));
 // app use
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use('/remixicon', express.static('node_modules/remixicon/fonts'));
+app.use('/remixicon', express.static('node_modules/remixicon'));
+
 
 // middleware
 app.use(cors());
